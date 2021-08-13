f4657c5a72b4615c9db3637e9b1188fe4593d14e
Junda Huang <1124345597@qq.com>
2021-08-13 14:32:45 +0800

    enabled python exception throw; finished c++ interface

--- scripts/Endeffector_serial_monitor.py
+++ scripts/Endeffector_serial_monitor.py
@@ -2,16 +2,24 @@ import os
 import serial
 import time
 
-# if the system is windows
-if os.name == 'nt':
-    ser = serial.Serial('COM6', 9600)
-else:
-    ser = serial.Serial('/dev/ttyUSB0', 9600)
-time.sleep(2)
+def detecting_Serial():
 
-b = 0
-while True:
-    b = ser.readline()
-    print (b)
+    while True:
+        try:
+            if os.name == 'nt':# if the system is windows
+                ser = serial.Serial('COM6', 9600)
+            else:
+                ser = serial.Serial('/dev/ttyUSB0', 9600)
+        except IOError:
+            print("[ERROR]: cannot open /dev/ttyUSB0, no such file or permission denied")
+        else:
+            if ser.is_open:
+                break
+        time.sleep(2)
+
+    b = 0
+    while True:
+        b = ser.readline()
+        print (b)
 
 
