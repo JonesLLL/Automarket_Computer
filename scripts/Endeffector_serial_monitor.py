3a1bdfb9ae8cc1f8a95fda2473de112d94819dc0
JonesLLL <87405335+JonesLLL@users.noreply.github.com>
2021-08-13 16:24:44 +0800

    print binary

--- scripts/Endeffector_serial_monitor.py
+++ scripts/Endeffector_serial_monitor.py
@@ -1,25 +1,29 @@
 import os
 import serial
 import time
+ser = serial.Serial('COM6', 9600)
+
+A = "Start system"
+print(A)
 
 def detecting_Serial():
 
-    while True:
-        try:
-            if os.name == 'nt':# if the system is windows
-                ser = serial.Serial('COM6', 9600)
-            else:
-                ser = serial.Serial('/dev/ttyUSB0', 9600)
-        except IOError:
-            print("[ERROR]: cannot open /dev/ttyUSB0, no such file or permission denied")
-        else:
-            if ser.is_open:
-                break
-        time.sleep(2)
+    # while True:
+    #     try:
+    #         if os.name == 'nt':# if the system is windows
+    #             ser = serial.Serial('COM6', 9600)
+    #         else:
+    #             ser = serial.Serial('/dev/ttyUSB0', 9600)
+    #     except IOError:
+    #         print("[ERROR]: cannot open /dev/ttyUSB0, no such file or permission denied")
+    #     else:
+    #         if ser.is_open:
+    #             break
+    #     time.sleep(2)
 
-    b = 0
+    # b = 0
     while True:
         b = ser.readline()
         print (b)
 
-
+detecting_Serial()
